e4fac0896e596a7a89702694183ae2368b3d424b
Thomas Sarlandie <thomas@sarlandie.net>
2014-06-16 20:37:55 -0700

    Back to pipe but with the user_env: true option

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -711,10 +711,10 @@ params = CGI.parse(uri.query || "")
 
   def generate_jekyll_site
     puts("Build compass CSS")
-    run!("env PATH=$PATH vendor/bundle/ruby/1.9.1/bin/compass compile --output-style compressed --force --css-dir source/stylesheets")
+    pipe("env PATH=$PATH vendor/bundle/ruby/1.9.1/bin/compass compile --output-style compressed --force --css-dir source/stylesheets", user_env: true)
 
     puts "Building jekyll site"
-    run!("env PATH=$PATH vendor/bundle/ruby/1.9.1/bin/jekyll build 2>&1")
+    pipe("env PATH=$PATH vendor/bundle/ruby/1.9.1/bin/jekyll build --trace 2>&1", user_env: true)
 
     unless $? == 0
       error "Failed to generate site with jekyll."
